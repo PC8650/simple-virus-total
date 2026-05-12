8bdd06ec9ce6af0033c825099cc79fa415910412
pc8650 <pc8650@protonmail.com>
2026-05-12 21:11:25 +0800

    update：配置调整

--- src/main/java/com/vt/config/HttpConfig.java
+++ src/main/java/com/vt/config/HttpConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HttpConfig {
 
-    @Value("${params.v-key:}")
+    @Value("${vt-key:}")
     private String apiKey;
 
     @Bean
